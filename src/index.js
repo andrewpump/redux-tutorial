0e0aecd0fb5a61e53bf13adf689e29fd3081f8d5
andrewpump <andrewh@pumpml.com>
2023-06-11 19:58:52 -0400

    Lodash compose and pipe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { compose, pipe } from 'lodash/fp';
+
 // non functional programming
 let input = "    Javascript   "
 let output = "<div>" + input.trim() + "</div>";
@@ -8,5 +10,17 @@ const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`
 const toLowerCase = str => str.toLowerCase();
 
-// function composition
+// function composition without Lodash compose
 const trimAndWrap = input => wrapInDiv(toLowerCase(trim(input)))
+
+// composes the functions from left to right
+const transform = compose(wrapInDiv, toLowerCase, trim)
+transform(input);
+
+const transform2 = pipe(trim, toLowerCase, wrapInDiv)
+
+console.log("Trim and Wrap: ", trimAndWrap(input))
+console.log("Transform w/ compose: ", transform(input))
+console.log("Transform w/ pipe: ", transform2(input))
+
+
