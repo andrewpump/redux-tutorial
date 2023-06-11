1372ff0a7c59cc5ea0e66e20c5a262c52c0e39b8
andrewpump <andrewh@pumpml.com>
2023-06-11 19:52:18 -0400

    function composition

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
+// non functional programming
+let input = "    Javascript   "
+let output = "<div>" + input.trim() + "</div>";
 
-// In javascript functions are first class citizens
-// This means that functions can be treated like any other variable, for example:
-// 1. A function can be assigned to a variable
-// 2. A function can be passed as an argument to another function
-// 3. A function can be returned from another function
+// functional programming
+// 1. trim    2. wrapInDiv
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`
+const toLowerCase = str => str.toLowerCase();
 
-function sayHello() {
-    return function() {
-        return "Hello World!";
-    }
-}
-
-function greet(fnMessage) {
-    console.log(fnMessage());
-}
-
-greet(sayHello);
-
-// Higher order functions either take a function as an argument or return a function as a result
-// Map, filter or reduce are all examples of higher order functions
+// function composition
+const trimAndWrap = input => wrapInDiv(toLowerCase(trim(input)))
