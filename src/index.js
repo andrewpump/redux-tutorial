7e1aa0b2d510447f42b770e3d45e6f505001860b
andrewpump <andrewh@pumpml.com>
2023-06-11 20:11:19 -0400

    pure functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 // A function is pure if given the same args, it gives the same result
 // All reducers in redux must be pure
+// pure functions are self documenting, increases testability, can easily be run concurrently, and cacheable (dynamic programming)
