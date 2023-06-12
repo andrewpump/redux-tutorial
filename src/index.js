0fd7750d1b2ca7a069226cbbcefe8e9398b197d6
andrewpump <andrewh@pumpml.com>
2023-06-11 20:10:24 -0400

    currying

--- src/index.js
+++ src/index.js
@@ -1,26 +1,2 @@
-import { compose, pipe } from 'lodash/fp';
-
-// non functional programming
-let input = "    Javascript   "
-let output = "<div>" + input.trim() + "</div>";
-
-// functional programming
-// 1. trim    2. wrapInDiv
-const trim = str => str.trim();
-const wrapInDiv = str => `<div>${str}</div>`
-const toLowerCase = str => str.toLowerCase();
-
-// function composition without Lodash compose
-const trimAndWrap = input => wrapInDiv(toLowerCase(trim(input)))
-
-// composes the functions from left to right
-const transform = compose(wrapInDiv, toLowerCase, trim)
-transform(input);
-
-const transform2 = pipe(trim, toLowerCase, wrapInDiv)
-
-console.log("Trim and Wrap: ", trimAndWrap(input))
-console.log("Transform w/ compose: ", transform(input))
-console.log("Transform w/ pipe: ", transform2(input))
-
-
+// A function is pure if given the same args, it gives the same result
+// All reducers in redux must be pure
